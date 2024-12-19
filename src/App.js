1a6e604a21934ea8e968183aa0f5abfe5558d645
abhishek17551 <ababhishek113@gmail.com>
2024-12-19 17:31:39 +0530

    explore GET,POST,DELETE https methods

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ const express = require('express')
 
 const app = express()
 
-app.use("/",(req,res) => {
-    res.send('Hello! Welcome to the Dashboard!')
+
+
+app.get("/wishlist",(req,res) => {
+    res.send({productID : '3', name : "Red Tape Shoes"})
 })
 
-app.use("/wishlist",(req,res) => {
-    res.send("Hello! Welcome to the Wishlist Page!")
+app.post("/wishlist",(req,res) => {
+    res.send("Product added to wishlist!")
 })
 
-app.use("/cart",(req,res) => {
-    res.send('Hello! Welcome to the Cart Page!')
+app.get("/cart",(req,res) => {
+    res.send({productID : '2', name : "HRX Shoes"})
 })
 
-app.use("/checkout",(req,res) => {
+app.post("/cart",(req,res) => {
+    res.send("Product added to cart!")
+})
+
+app.delete("/cart",(req,res) => {
+    res.send("Product removed from Cart!")
+})
+
+app.get("/checkout",(req,res) => {
     res.send('Hello! Welcome to the Checkout Page!')
+})
+
+app.get("/",(req,res) => {
+    res.send('Hello! Welcome to the Dashboard!')
 })
 
 app.listen(3000, () => {

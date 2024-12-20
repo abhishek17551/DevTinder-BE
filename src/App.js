17b5fe38ee2c428519ce76838d1647e602054812
abhishek17551 <ababhishek113@gmail.com>
2024-12-20 16:20:32 +0530

    multiple route handlers

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ const express = require('express')
 
 const app = express()
 
-
+app.get("/user",
+    (req,res,next) => {
+    console.log('Handling the user route 1')
+    res.send('1st response')
+    
+    next()
+},
+(req,res,next) => {
+   
+    console.log('Handling the user route 2')
+    res.send('2nd response')
+    next()
+},
+(req,res) => {
+    console.log('Handling the user route 3')
+    res.send('3rd response')
+},
+       
+)
 
 app.get("/wishlist",(req,res) => {
     res.send({productID : '3', name : "Red Tape Shoes"})
@@ -12,13 +30,18 @@ app.post("/wishlist",(req,res) => {
     res.send("Product added to wishlist!")
 })
 
+app.get("/cart/:productID",(req,res) => {
+    console.log(req.params)
+    res.send({productID : '2', name : "HRX Shoes"})
+})
+
 app.get("/cart",(req,res) => {
     res.send({productID : '2', name : "HRX Shoes"})
 })
 
 app.post("/cart",(req,res) => {
     res.send("Product added to cart!")
-})
+} )
 
 app.delete("/cart",(req,res) => {
     res.send("Product removed from Cart!")

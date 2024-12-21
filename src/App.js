79b25b0c7e268970f2f222e97fbeb0799f4fe202
abhishek17551 <ababhishek113@gmail.com>
2024-12-21 14:39:21 +0530

    created GET apis for getting - user by email,all feeds

--- src/App.js
+++ src/App.js
@@ -18,7 +18,34 @@ app.post("/signup", async (req,res)=>{
     catch(err){
         res.status(400).send('Error in adding User:'+err.message)
     }
-})                                                                                  
+})  
+
+//Get user by emailId
+app.get('/user', async (req,res) => {
+    const userEmail = req.body.emailId
+
+    try {
+        const user = await User.findOne({emailId:userEmail})
+        if(!user){
+            res.status(404).send("User not found")
+        } else {
+            res.send(user)
+        }
+    }
+    catch(err){
+        res.status(400).send('Something went wrong!!!')
+    }
+})
+
+//Get all feed
+app.get('/feed', async (req,res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }    catch(err){
+        res.status(400).send('Something went wrong!!!')
+    }
+})
 
 
 connectDB()

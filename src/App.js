3e5ab26958664da6e9d6f2b5f7aff49fd011cc93
abhishek17551 <ababhishek113@gmail.com>
2024-12-21 13:44:05 +0530

    created POST req for signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 const express = require('express')
-const  connectDB = require('./config/database')
+const connectDB = require('./config/database')
+const User = require('./models/user')
 const app = express()
 
 
-connectDB().then(() => {
-    console.log('DB connection established')
-    app.listen(3000, () => {
-        console.log('Server is listening for requests on port 3000')
+
+app.post("/signup", async (req,res)=>{  
+    //Creating a new instance of User model
+    const user = new User({
+        firstName:"AB De",
+        lastName:"Villiers",
+        email : "abdev@gmail.com",
+        password : "abdev@123"
     })
-}).catch((err) => {
-    console.log('Error in connecting to the DB', err)
-})
+    try {
+        await user.save()
+        res.status(201).send("User added successfully");
+    }
+    catch(err){
+        res.status(400).send('Error in adding User:'+err.message)
+    }
+})                                                                                  
+
+
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening on port 7777...");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!");
+  });
 

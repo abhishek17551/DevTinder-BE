4fcf182a70f48880d66e48f7c9af51078061d449
abhishek17551 <ababhishek113@gmail.com>
2024-12-23 20:30:04 +0530

    refactor:created express route for '/profile'

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,10 +4,9 @@ const profileRouter = express.Router()
 const { userAuth } = require('../middlewares/auth')
 
 
-profileRouter.get("/profile/view", userAuth, async (req, res) => {
+profileRouter.get("/profile", userAuth, async (req, res) => {
     try {
       const user = req.user;
-  
       res.send(user);
     } catch (err) {
       res.status(400).send("ERROR : " + err.message);

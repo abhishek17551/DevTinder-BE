bf39b2086dd8705068b48ca3f484fb3c5feb55b0
abhishek17551 <ababhishek113@gmail.com>
2025-01-31 20:53:19 +0530

    defined functionality to save chat to DB

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io")
 const crypto = require("crypto");
+const Chat = require("../models/chat");
 
 const getSecretRoomId = (userId,targetUserId) => {
     return crypto.createHash("sha256").update([userId,targetUserId].sort().join("$")).digest("hex")
@@ -18,10 +19,32 @@ const initializeSocket = (server) => {
           console.log(firstName + " joined Room : " + roomId)
           socket.join(roomId)
       })
-      socket.on("sendMessage", ({firstName,userId,targetUserId,text}) => {
-          const roomId = getSecretRoomId(userId,targetUserId)
-          console.log(firstName + " : " + text);
-          io.to(roomId).emit("messageReceived", {firstName,text})
+      socket.on("sendMessage", async ({firstName,userId,targetUserId,text}) => {
+          try {
+            const roomId = getSecretRoomId(userId, targetUserId);
+
+            let chat = Chat.findOne({
+              participants : {$all : [userId,targetUserId]}
+            })
+
+            if(!chat) {
+              chat = new Chat({
+                participants : [userId,targetUserId],
+                messages : []
+              })
+            }
+
+            chat.messages.push({
+              senderId : userId,
+              text
+            })
+
+            await chat.save()
+            io.to(roomId).emit("messageReceived", { firstName, lastName, text });
+          }
+          catch (err) {
+            console.log(err);
+          }
     })
       socket.on("disconnect", () => {})
     })
